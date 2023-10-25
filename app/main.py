303110f3a451a16e4535604b2a71ebd0d75644d2
kettererdavid <david.kette@gmail.com>
2023-10-25 08:03:24 -0600

    processing both files simultaneouly and creating combined xlsx with 2 sheets
    new template to accomodate new functionality

--- app/main.py
+++ app/main.py
@@ -41,57 +41,6 @@ def upload_file():
     # Send the file for download
     return send_file(output, as_attachment=True, download_name='combined_report.xlsx',
                      mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-# def upload_file():
-#     # Handle file1 input
-#     if 'file1' in request.files:
-#         file = request.files['file1']
-#
-#         # Check if a file was provided
-#         if file.filename == '':
-#             return 'No file provided for Synlab'
-#
-#         # Check if the file is allowed
-#         if file and allowed_file(file.filename):
-#             file.save(file.filename)
-#             df = pd.concat([synname(file.filename), synpage1(file.filename), synpage2(file.filename),
-#                             synpage3(file.filename)],
-#                            axis=0)
-#
-#             output = BytesIO()
-#             df.to_excel(output, index=False)
-#             output.seek(0)
-#
-#             return send_file(output, as_attachment=True, download_name=f'Synlab_{synname(file.filename).columns[0]}.xlsx',
-#                              mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-#
-#     elif 'file2' in request.files:
-#         file = request.files['file2']
-#         # Check if a file was provided
-#         if file.filename == '':
-#             return 'No file provided for Enterosan'
-#
-#         # Check if the file is allowed
-#         if file and allowed_file(file.filename):
-#             file.save(file.filename)
-#             df = pd.concat([enteroname(file.filename), enteropage1(file.filename), enteropage2(file.filename)],
-#                            axis=0)
-#
-#             output = BytesIO()
-#             df.to_excel(output, index=False)
-#             output.seek(0)
-#
-#             return send_file(output, as_attachment=True, download_name=f'Enterosan_{enteroname(file.filename).columns[0]}.xlsx',
-#                              mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-#
-#     elif 'file3' in request.files:
-#         file = request.files['file3']
-#         # TODO: Implement processing for file3
-#         return 'File3 processing not yet implemented'
-#
-#     else:
-#         return 'No valid file input provided'
-#
-#     return render_template('upload2.html')
 
 
 @APP.route('/')
