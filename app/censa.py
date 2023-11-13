42c96f591d059a3db852c5189acfbe7d18ce95f0
kettererdavid <david.kette@gmail.com>
2023-11-13 09:26:16 -0700

    Added reference value column to censa report

--- app/censa.py
+++ app/censa.py
@@ -3,6 +3,6 @@ import tabula
 
 def censa1(input_path):
     page1 = tabula.read_pdf(input_path, pages="1")[0]
-    page1 = page1[['Laborparameter', 'e  s  swert']]
+    page1 = page1[['Laborparameter', 'e  s  swert', 'Vergleichsbereich']]
     page1.rename(columns={'e  s  swert': 'Messwert'}, inplace=True)
     return page1
