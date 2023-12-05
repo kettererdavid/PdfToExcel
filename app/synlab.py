def4ca80110052ec447ad14db869fe74c115abd5
kettererdavid <david.kette@gmail.com>
2023-12-05 08:30:04 -0700

    Increased readout box length to accommodate for more results on each page

--- app/synlab.py
+++ app/synlab.py
@@ -18,7 +18,7 @@ def synpage1(input_path):
 def synpage2(input_path):
     pdf_info = tabula.read_pdf(input_path, pages='all', multiple_tables=True, stream=True, output_format='json')
     if len(pdf_info) > 1:
-        area = [145, 50, 725, 300]
+        area = [145, 50, 750, 300]
         page1 = tabula.read_pdf(input_path, pages="2", area=area)
         page1 = page1[0]
         page1.rename(columns={'Unnamed: 0': 'Alles'}, inplace=True)
@@ -44,7 +44,7 @@ def synpage2(input_path):
 def synpage3(input_path):
     pdf_info = tabula.read_pdf(input_path, pages='all', multiple_tables=True, stream=True, output_format='json')
     if len(pdf_info) > 2:
-        area = [145, 50, 595, 300]
+        area = [145, 50, 750, 300]
         page1 = tabula.read_pdf(input_path, pages="3", area=area)
         page1 = page1[0]
         page1.rename(columns={'Unnamed: 0': 'Alles'}, inplace=True)
